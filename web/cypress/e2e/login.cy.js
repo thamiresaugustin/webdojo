d1ecb523a5038df93cb325696a6623c70db12ebe
thamiresaugustin <thamiresaugustinn@gmail.com>
2026-01-26 22:14:10 -0300

    cases login

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,14 +1,32 @@
-
-//describe para agrupar testes
-//it é o caso de teste
 describe('Login', ()=>{
   it('Deve logar com sucesso', ()=>{
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana123')
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
 
-    cy.contains('button', 'Entrar').click()
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
   })
+
+    it('Não deve logar com senha inválida', ()=>{
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana321')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+    it('Não deve logar com email não cadastrado', ()=>{
+    cy.start()
+    cy.submitLoginForm('404@webdojo.com', 'katana123')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+
 })
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('start',()=> {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+})
+
+Cypress.Commands.add('submitLoginForm', (email, senha)=> {
+    cy.get('#email').type(email)
+    cy.get('#password').type(senha)
+
+    cy.contains('button', 'Entrar').click()
+})
